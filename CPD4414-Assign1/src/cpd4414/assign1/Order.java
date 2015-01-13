794aca58abd895ec3170ee344ff9c3dcf8378ac2
Scott Melanson <scott_melanson@outlook.com>
2015-01-13 13:02:11 -0500

    Created File Order.java

---
+++ CPD4414-Assign1/src/cpd4414/assign1/Order.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cpd4414.assign1;
+
+/**
+ *
+ * @author Scott Melanson
+ */
+public class Order {
+    
+    private String customerId;
+    private String customerName;
+    private String timeRecieved;
+    private String timeProcessed;
+    private String timeFulfilled;
+    private String notes;
+    
+}
